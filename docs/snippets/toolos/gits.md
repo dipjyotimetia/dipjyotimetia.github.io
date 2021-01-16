2d0ce51033004e488657ce5cd0a1ef10a809a4f5
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-01-17 00:31:57 +1100

    bugfix

--- docs/snippets/toolos/gits.md
+++ docs/snippets/toolos/gits.md
@@ -1,5 +1,5 @@
 ---
-id: github
+id: gits
 title: Git Cheat sheet
 ---
 
--- sidebars.js
+++ sidebars.js
@@ -69,7 +69,7 @@ module.exports = {
       type: 'category',
       label: 'OS & Tools',
       items: [
-        'snippets/toolos/github',
+        'snippets/toolos/gits',
         'snippets/toolos/ubuntu',
       ]
     },
